7d0f6d05c2fd5f3c3d352dec53fd946ad3385a72
Léo <legrivel@student.le-101.fr>
2018-05-15 01:55:35 +0200

    PR review

--- client/src/app/components/Media.js
+++ client/src/app/components/Media.js
@@ -7,6 +7,7 @@ class Media extends Component {
     constructor(props) {
         super(props);
 
+        this.admins = ["legrivel", "jmarquet"];
         this.deleteMedia = this.deleteMedia.bind(this);
     }
 
@@ -44,7 +45,7 @@ class Media extends Component {
             <div className={"mediaContainer"}>
                 <div className={"subMediaContainer"}>
                     {this.renderType()}
-                    {(this.props.login === this.props.media.author || this.props.login === "legrivel" || this.props.login === "jmarquet") &&
+                    {(this.props.login === this.props.media.author || this.admins.indexOf(this.props.login) !== -1) &&
                     <svg onClick={this.deleteMedia} className={"deleteIcon"} enableBackground="new 0 0 32 32" height="32px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="32px"><path d="M20.377,16.519l6.567-6.566c0.962-0.963,0.962-2.539,0-3.502l-0.876-0.875c-0.963-0.964-2.539-0.964-3.501,0  L16,12.142L9.433,5.575c-0.962-0.963-2.538-0.963-3.501,0L5.056,6.45c-0.962,0.963-0.962,2.539,0,3.502l6.566,6.566l-6.566,6.567  c-0.962,0.963-0.962,2.538,0,3.501l0.876,0.876c0.963,0.963,2.539,0.963,3.501,0L16,20.896l6.567,6.566  c0.962,0.963,2.538,0.963,3.501,0l0.876-0.876c0.962-0.963,0.962-2.538,0-3.501L20.377,16.519z" fill="#515151"/></svg>}
                 </div>
             </div>
