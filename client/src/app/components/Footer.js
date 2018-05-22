7b7d1b6f1292cb130fda0e9d2f4dd8536634af28
PCJULIEN\Julien_Marquet <julienmarquet9@gmail.com>
2018-05-22 02:07:32 +0200

    Add basic footer

--- client/src/app/components/Footer.js
+++ client/src/app/components/Footer.js
@@ -5,6 +5,34 @@ class Footer extends Component {
     render() {
         return (
             <div className={"footer"}>
+                <div className={"footerFlex"}>
+                    <div className={"footerCol terms"}>
+                        <p><a>Terms of use</a></p>
+                    </div>
+                    <div className={"footerCol creators"}>
+                        <div className={"footerColWrapper"}>
+                            <div className={"creator"}>
+                                <div className={"creatorProfileWrapper"}>
+                                    <a className={"creatorProfile"}>
+                                        Julien Marquet
+                                    </a>
+                                </div>
+                                <button className={"creatorButton"}>GIT</button>
+                                <button className={"creatorButton"}>LNK</button>
+                            </div>
+                            <div className={"creator"}>
+                                <div className={"creatorProfileWrapper"}>
+                                    <a className={"creatorProfile"}>
+                                        Léo Grivel
+                                    </a>
+                                </div>
+                                <button className={"creatorButton"}>GIT</button>
+                                <button className={"creatorButton"}>LNK</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={"footerResp"} />
             </div>
         );
     }
