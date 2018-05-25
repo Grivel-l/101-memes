97400b6fcccb94707d3fc24b8110b6778aa0a298
Leo Grivel <legrivel@student.le-101.fr>
2018-05-25 17:30:47 +0200

    Added terms

--- client/src/app/components/Footer.js
+++ client/src/app/components/Footer.js
@@ -4,6 +4,10 @@ import "../scss/footer.css";
 class Footer extends Component {  
     constructor(props) {
         super(props);
+
+        this.state = {
+            showTerms: false
+        };
         this.svgSize = 40;
     }
 
@@ -34,13 +38,51 @@ class Footer extends Component {
         );
     }
 
+    renderTerms() {
+        return (
+            <div className={this.state.showTerms ? "termsWrapper showTerms" : "termsWrapper"} onClick={() => this.setState({showTerms: !this.state.showTerms})}>
+                <p>{"This terms have been greatly inpired by those of imgur.com. \
+The use of this application is reserved exclusively for users with an active account on intra.42.fr. \
+Using our app to do anything other than access the links to this \"Terms\" page or accessing any page of our application other than this \"Terms\" page constitutes your consent to these terms of use and to our Privacy Policy. If you do not consent, do not use our application. \
+Your use of our application to do anything beyond simply accessing/viewing it (that is, uploading, downloading, etc.), constitutes not merely your consent, but also your electronic signature, meaning that you are contractually bound by these terms and by our Privacy Policy. \
+Stuff not to do \
+If someone else might own the copyright to it, don't upload it. Don't upload gore, \"hate speech\" (i.e. demeaning race, gender, age, religious or sexual orientation, etc.), or material that is threatening, harassing, defamatory, or that encourages violence or crime. Don't upload illegal content such as child porn or nonconsensual (\"revenge\") porn. Don't hotlink to adult content or to file-sharing, gambling, torrent, warez sites. Don't impersonate someone else. Also, don't use 101_memes to host image libraries you link to from elsewhere, content for your website, advertising, avatars, or anything else that turns us into your content delivery network. If you do or if you do anything illegal, in addition to any other legal rights we may have, we will ban delete all your images, report you to the authorities if necessary, and prevent you from viewing any images hosted on 101_memes. \
+Stuff to do \
+If you see anything on our site that shouldn't be there because it violates our policies, is illegal (e.g. revenge porn or child porn), or for some other reason, please let us know by emailing us at « email de moderation » or by contacting an admin or a moderator. \
+About images you upload \
+By uploading images online you accept that any other user can access and share them. \
+ \
+PRIVACY POLICY \
+ \
+Information we collect and how we use it \
+Automatically collected technical information: Our servers can log information about each computer connecting with our site such as IP address, dates and times of each login, browser type, type of connection, page and image viewing statistics. We also log the metadata associated with any images or videos you upload. None of this automatically collected technical information is associated with any identified person at the time it is collected, but it could be associated with you if we are required to disclose our server logs as a result of a subpoena or other legal process, some third party such as your internet provider could match our anonymous technical information with you, using information beyond what is found on our servers. \
+We may use cookies, web beacons, or other anonymous tracking information to improve our server's interaction with your computer. These cookies are required in order for the application to function properly. \
+Personal Data Protection \
+We store no other personnal data than those hosted on your intra42 profile. \
+Image Privacy \
+Every image uploaded to 101_meme can always be accessed and viewed by any logged user. \
+ \
+Community rules \
+ \
+If you see an image or a title that breaks the following, we welcome you to report it to moderators. \
+If content breaks these community rules, it may be removed and the original poster warned about the removal. Warnings will expire. However, if multiple submissions break the rules in a short time frame, warnings will accumulate, which could lead the concerned user to be added to a blacklist preventing him from accessing any content hosted on 101_memes. \
+•    No nudity or sexually explicit, provocative, inflammatory, unsettling, or suggestive posts. \
+•    No hate speech, abuse, or harassment. \
+•    No content that condones illegal or violent activity. \
+•    No gore or mutilation. \
+•    No spam or prohibited behavior. \
+•    No posts that violate 101_memes terms of service."}</p>
+            </div>
+        );
+    }
+
     render() {
         return (
             <div className={"footer"}>
                 <div className={"footerFlex"}>
                     <div className={"footerCol terms"}>
                         <div className={"footerColWrapper"}>
-                            <p><a>Terms of use</a></p>
+                            <p className={"terms"} onClick={() => this.setState({showTerms: !this.state.showTerms})}>Terms of use</p>
                         </div>
                     </div>
                     <div className={"footerCol creators"}>
@@ -63,6 +105,7 @@ class Footer extends Component {
                     </div>
                 </div>
                 <div className={"footerResp"} />
+                {this.renderTerms()}
             </div>
         );
     }
