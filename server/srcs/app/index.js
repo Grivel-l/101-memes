8ecee7e7c75521b9fe7ceff8f559e3b9de73ebe2
Léo <legrivel@student.le-101.fr>
2018-06-03 00:36:21 +0200

    Routes wern't loaded if dtb wern't launched

--- server/srcs/app/index.js
+++ server/srcs/app/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const restify = require("restify");
 const bunyan = require("bunyan");
+const mongoose = require("mongoose");
 const corsMiddleware = require("restify-cors-middleware");
 
 const routes = require("./routes/");
@@ -34,41 +35,50 @@ server.use((req, res, next) => {
     next();
 });
 server.use((req, res, next) => apiHelper.checkToken(req, res, next, log, globalUsers));
+
+let serverInited = false;
+function initServer(mongoose) {
+    if (serverInited) {
+        return false;
+    }
+    serverInited = true;
+    require("./models/schemas/index")(mongoose);
+    const users = new UsersController(mongoose);
+    return users.updateUsers(globalUsers)
+        .then(() => {
+            process.stdin.setEncoding("utf8");
+            process.stdin.on("data", text => {
+                text = text.split(" ");
+                if (text[0] === "setUser") {
+                    if (text.length !== 3) {
+                        return log.error("Usage: setUser login role");
+                    }
+                    users.insertUser(text[1], text[2].trim(), globalUsers)
+                        .then(() => log.info("User list updated: ", globalUsers))
+                        .catch(error => log.error(error));
+                } else if (text[0] === "deleteUser") {
+                    if (text.length !== 2) {
+                        return log.error("Usage: deleteUser login");
+                    }
+                    users.deleteUser(text[1].trim(), globalUsers)
+                        .then(() => log.info("User list update: ", globalUsers))
+                        .catch(error => log.error(error));
+                } else if (text[0].trim() === "listUsers") {
+                    if (text.length !== 1) {
+                        return log.error("Usage: listUsers");
+                    }
+                    log.info("", globalUsers);
+                }
+            });
+            routes(server, restify.plugins, log, mongoose, globalUsers);
+            log.info("Routes loaded");
+        });
+}
+
+mongoose.connection.on("connected", () => initServer(mongoose));
 server.listen(8080, () => {
     log.info(`Server listening at ${server.url}`);
     dtb.init()
-        .then(mongoose => {
-            require("./models/schemas/index")(mongoose);
-            const users = new UsersController(mongoose);
-            return users.updateUsers(globalUsers)
-                .then(() => {
-                    process.stdin.setEncoding("utf8");
-                    process.stdin.on("data", text => {
-                        text = text.split(" ");
-                        if (text[0] === "setUser") {
-                            if (text.length !== 3) {
-                                return log.error("Usage: setUser login role");
-                            }
-                            users.insertUser(text[1], text[2].trim(), globalUsers)
-                                .then(() => log.info("User list updated: ", globalUsers))
-                                .catch(error => log.error(error));
-                        } else if (text[0] === "deleteUser") {
-                            if (text.length !== 2) {
-                                return log.error("Usage: deleteUser login");
-                            }
-                            users.deleteUser(text[1].trim(), globalUsers)
-                                .then(() => log.info("User list update: ", globalUsers))
-                                .catch(error => log.error(error));
-                        } else if (text[0].trim() === "listUsers") {
-                            if (text.length !== 1) {
-                                return log.error("Usage: listUsers");
-                            }
-                            log.info("", globalUsers);
-                        }
-                    });
-                    routes(server, restify.plugins, log, mongoose, globalUsers);
-                    log.info("Routes loaded");
-                });
-        })
+        .then(initServer)
         .catch(error => log.error(error));
 });
