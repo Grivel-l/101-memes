33f774b1b8e00890e2e3f261732ac4d6d4740c53
Julien Marquet <jmarquet@z2r3p3.le-101.fr>
2018-06-05 17:18:32 +0200

    Add tags to model

--- server/srcs/app/models/schemas/Medias.schema.js
+++ server/srcs/app/models/schemas/Medias.schema.js
@@ -4,7 +4,16 @@ module.exports = dtb => {
         path: {type: String, required: true},
         author: {type: String, required: true},
         type: {type: String, enum: ["video/mp4", "video/webm", "image/jpg", "image/jpeg", "image/png", "image/gif"], required: true},
+        tags: {
+            type: Array,
+            of: String,
+            validate: (val) =>  {
+                return (val.length <= 3);
+            },
+        },
         deleted: {type: Boolean, default: false},
         createDate: {type: Date, required: true}
     }));
 };
+
+
