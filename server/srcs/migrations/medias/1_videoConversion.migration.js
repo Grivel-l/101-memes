d3510c034e3e463ecffcf2d05ed82ca22bbc5141
Leo Grivel <legrivel@student.le-101.fr>
2018-06-12 20:18:07 +0200

    Added console.log

--- server/srcs/migrations/medias/1_videoConversion.migration.js
+++ server/srcs/migrations/medias/1_videoConversion.migration.js
@@ -11,6 +11,7 @@ module.exports = {
             files.map(file => {
                 const ext = file.split(".")[file.split(".").length - 1];
                 if (["mp4", "webm", "gif"].includes(ext)) {
+                    console.log(`Converting ${file}...`);
                     promises.push(mediasHelper.convertVideo(ext, file.substring(0, file.length - (ext.length + 1)), "./srcs/imgs/"));
                 }
             });
