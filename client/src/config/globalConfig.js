e22fd19488780430896856a549e10dfde0babd08
Grivel-l <legrivel@student.le-101.fr>
2020-10-12 18:12:24 +0200

    Bind correct addresses

--- client/src/config/globalConfig.js
+++ client/src/config/globalConfig.js
@@ -1,5 +1,5 @@
 export default {
-    serverEndpoint: "http://localhost:8080",
+    serverEndpoint: "http://server:8080",
     clientUrl: "http://localhost:3000",
     redirectionUrl: "https://the-matrix.le-101.fr",
     maxFileSize: 10, // mo
